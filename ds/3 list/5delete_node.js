2b55a60d6488865ce553989be44637e472175bbb
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-12-07 17:08:17 +0530

    Add files via upload

---
+++ ds/3 list/5delete_node.js
@@ -0,0 +1,29 @@
+class LinkedList {
+    // ... (previous code)
+  
+    deleteNode(data) {
+      if (!this.head) {
+        return;
+      }
+  
+      if (this.head.data === data) {
+        this.head = this.head.next;
+        return;
+      }
+  
+      let current = this.head;
+      let prev = null;
+  
+      while (current && current.data !== data) {
+        prev = current;
+        current = current.next;
+      }
+  
+      if (!current) {
+        return;
+      }
+  
+      prev.next = current.next;
+    }
+  }
+  
