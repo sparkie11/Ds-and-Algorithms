75355a17c3bf526c465d175485106eeb2ada1c76
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-12-07 17:08:32 +0530

    Add files via upload

---
+++ ds/3 list/6reverse_ll.js
@@ -0,0 +1,19 @@
+class LinkedList {
+    // ... (previous code)
+  
+    reverse() {
+      let prev = null;
+      let current = this.head;
+      let next = null;
+  
+      while (current) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+      }
+  
+      this.head = prev;
+    }
+  }
+  
