a5f4a37dd03b5c14f9e3c4d1bf4601a07b782a18
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-12-07 17:07:04 +0530

    Add files via upload

---
+++ ds/3 list/2linklist.js
@@ -0,0 +1,20 @@
+class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    addNode(data) {
+      const newNode = new Node(data);
+  
+      if (!this.head) {
+        this.head = newNode;
+      } else {
+        let current = this.head;
+        while (current.next) {
+          current = current.next;
+        }
+        current.next = newNode;
+      }
+    }
+  }
+  
