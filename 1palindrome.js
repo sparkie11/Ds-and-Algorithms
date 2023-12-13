32f361165bb66e679e80105151bda24b951c13ae
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-12-13 13:20:48 +0530

    Add files via upload

---
+++ 1palindrome.js
@@ -0,0 +1,29 @@
+// functions  push pop peek length
+
+
+// arrays are example of stack 
+
+
+var letters =[];
+
+var word="racecar"
+
+var rword = "";
+
+
+// lets put the word into the stack 
+
+for (var i=0; i < word.length;i++){
+    letters.push(word[i])
+}
+
+for (var i=0; i < word.length;i++){
+    rword += letters.pop();
+}
+
+if(rword === word){
+    console.log('it is palindrome');
+}
+else{
+    console.log('not');
+}
