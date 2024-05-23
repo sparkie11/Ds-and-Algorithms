372a831d60e8a348acb4c22447dd6e9879fc0b1d
Gautam Wise <122539340+sparkie11@users.noreply.github.com>
2024-05-24 00:12:38 +0530

    Update 1for.js

--- ds/1Array/1for.js
+++ ds/1Array/1for.js
@@ -1,4 +1,4 @@
-                                               // Today's  declaration  55 
+                                                // Today's  declaration  55 
 let numbers = [1, 2, 3, 4, 5];
 
 for (let i = 0; i < numbers.length; i++) {
