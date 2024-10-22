268741d126bdc96bb0396a901af00cc5790ee500
Gautam Wise <gautamwise.s@gmail.com>
2024-10-22 16:11:10 +0530

    Update hashtable.js

--- ds/5 hashtable/hashtable.js
+++ ds/5 hashtable/hashtable.js
@@ -1,4 +1,4 @@
-                                                                    /* Has h Table   now    not       */ 
+                                                                     /* Has h Table   now    not       */ 
 
 var hash = (string, max) => {
     var hash = 0;
